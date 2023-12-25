ae70c930ecde5d9422796e457afddbc2dea72090
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-12-25 12:51:58 +0530

    Added filters for status, type, and landings
    - Added filters to search through the grid.

---
+++ src/components/Capsules/CapsulesFilter/CapsulesFilter.jsx
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from "react-redux"
+import { isSearchFilterSet } from "../../../services/isSearchFilterSet"
+import { applyFilterReset, applySearchFilter, fetchCapsules, updateSearchFilter } from "../../../redux/actions"
+
+const CapsulesFilter = () => {
+  const dispatch = useDispatch()
+  const {dropdownValues, searchFilter, pageNumber} = useSelector(state => state.capsules)
+  
+  const updateAndApplyFilter = (filterKey, dropdownValue) => {
+    dispatch(updateSearchFilter(filterKey, dropdownValue))
+    dispatch(applySearchFilter(filterKey, dropdownValue))
+  }
+
+  const handleFilterReset = () => {
+    dispatch(applyFilterReset())
+    dispatch(fetchCapsules(pageNumber));
+  }
+
+  return (
+   <div className="flex">
+    {
+      Object.keys(dropdownValues).map(filterKey => 
+        <div key={filterKey}>
+          <select onChange={(e) => updateAndApplyFilter(filterKey, e.target.value)} id={filterKey} className="mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected disabled>Choose a {filterKey}</option>
+            {
+              dropdownValues[filterKey].map(dropdownOption => 
+                <option value={dropdownOption} key={dropdownOption}>{dropdownOption}</option>
+              )
+            }
+          </select>          
+        </div>  
+      )
+    }
+    {isSearchFilterSet(searchFilter) && <button onClick={handleFilterReset} className="text-gray-200 text-xl ms-1">x</button>}
+   </div>
+  )
+}
+
+export default CapsulesFilter
